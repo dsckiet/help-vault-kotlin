cc5af92155ba80977ffc8880c59557c2efffae4a
Aditya <icgadi99@gmail.com>
2020-10-28 21:48:57 +0530

    Added - Code for data binding, refactored fragment classes

--- app/src/main/java/com/dsckiet/helpvault/view/MainActivity.kt
+++ app/src/main/java/com/dsckiet/helpvault/view/MainActivity.kt
@@ -2,11 +2,14 @@ package com.dsckiet.helpvault.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.dsckiet.helpvault.R
+import com.dsckiet.helpvault.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
