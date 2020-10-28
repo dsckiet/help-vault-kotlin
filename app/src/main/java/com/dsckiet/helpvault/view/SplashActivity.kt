748bcb536d4fdc8f230ca328706eec1ba87385e5
Vidit Jha <jhavidit@gmail.com>
2020-10-28 11:24:53 +0530

    Added - Splash Activity

--- app/src/main/java/com/dsckiet/helpvault/view/SplashActivity.kt
+++ app/src/main/java/com/dsckiet/helpvault/view/SplashActivity.kt
@@ -2,10 +2,15 @@ package com.dsckiet.helpvault.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.dsckiet.helpvault.R
+import com.dsckiet.helpvault.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding:ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_splash)
     }
 }
