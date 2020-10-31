f7f225fcb70a78dba1919b3cbfda0a9bfcc0c1fe
Aditya <icgadi99@gmail.com>
2020-10-31 12:04:40 +0530

    Updated - Back button navigation from Help requests page

--- app/src/main/java/com/dsckiet/helpvault/view/HelpRequestsActivity.kt
+++ app/src/main/java/com/dsckiet/helpvault/view/HelpRequestsActivity.kt
@@ -1,7 +1,9 @@
 package com.dsckiet.helpvault.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.dsckiet.helpvault.R
 import com.dsckiet.helpvault.databinding.ActivityHelpRequestsBinding
 
@@ -9,7 +11,10 @@ class HelpRequestsActivity : AppCompatActivity() {
     private lateinit var binding : ActivityHelpRequestsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_help_requests)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_help_requests)
+        binding.backBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
