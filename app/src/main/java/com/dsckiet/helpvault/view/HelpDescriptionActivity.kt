0daf2c362654755d65e3412dc6add0547756a45d
Aditya <icgadi99@gmail.com>
2020-10-31 17:03:09 +0530

    Added - Help description page

---
+++ app/src/main/java/com/dsckiet/helpvault/view/HelpDescriptionActivity.kt
@@ -0,0 +1,12 @@
+package com.dsckiet.helpvault.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dsckiet.helpvault.R
+
+class HelpDescriptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_help_description)
+    }
+}
